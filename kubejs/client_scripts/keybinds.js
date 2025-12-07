b893058f285aad97677601d98da6316eb4c0d4d6
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2025-12-07 14:45:08 +0100

    feat: removed unused keybinds from recently added mods

--- kubejs/client_scripts/keybinds.js
+++ kubejs/client_scripts/keybinds.js
@@ -4,9 +4,19 @@ const keybindsToRemove = [
     'iris.keybind.wireframe',
     'key.modernfix.config',
     'key.entityculling.toggle',
+    'key.dynamic_fps.toggle_forced',
+    'key.dynamic_fps.toggle_disabled',
+    'key.inventoryessentials.single_transfer',
+    'key.inventoryessentials.screen_bulk_drop',
+    'key.inventoryessentials.sort_inventory',
+    'key.trashslot.toggle_lock',
+    'key.trashslot.toggle',
+    'key.fancytoasts.config_menu'
+   
 ]
 
 const keybindsToRebind = {
+    'key.advancements': { key: GLFW.GLFW_KEY_G},
     'iris.keybind.reload': { key: GLFW.GLFW_KEY_O, modifier: KeyModifier.SHIFT },
     'iris.keybind.toggleShaders': { key: GLFW.GLFW_KEY_O, modifier: KeyModifier.CONTROL },
 }
@@ -21,6 +31,6 @@ KeyBindJSEvents.modify(event => {
 
     for (const [key, { key: newKey, modifier }] of Object.entries(keybindsToRebind)) {
         event.modifyKey(key, newKey);
-        event.modifyModifier(key, modifier);
+        event.modifyModifier(key, modifier !== undefined ? modifier : KeyModifier.NONE);
     }
 })
