24e8060233cea6cf0c9b24186f815243fcbb2885
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2025-12-19 23:03:09 +0100

    refactor: Dyed Flames compat to handle case of missing mods

--- kubejs/server_scripts/dyed_flames.js
+++ kubejs/server_scripts/dyed_flames.js
@@ -1,21 +1,35 @@
+const createFireType = (particle, textureBase) => ({
+    particle: particle,
+    texture0: `${textureBase}_0`,
+    texture1: `${textureBase}_1`
+});
+
+const modFireTypes = {
+    "supplementaries": {
+        "lumisene": createFireType(
+            "supplementaries:sparkle",
+            "supplementaries:block/lumisene_fire"
+        ),
+    }
+}
+
 ServerEvents.generateData("after_mods", event => {
 
-    // let dyedFLamesJson = KJSTweaks.readJsonFromMod("dyedflames", "data_maps/block/fire_types")
+    let fireTypesData = {};
 
-    // console.log("Dyed Flames Fire Types:" + dyedFLamesJson)
+    for (const [modid, fireTypes] of Object.entries(modFireTypes)) {
+        if (!Platform.isLoaded(modid)) {
+            console.log(`Dyed Flames compat : Skipping fire types for ${modid} (mod not loaded)`)
+            continue
+        }
 
-    // Breaks every entry if trying to reference fire sources, particles or textures that don't exist
+        for (const [name, data] of Object.entries(fireTypes)) {
+            fireTypesData[`${modid}:${name}`] = data
+        }
+    }
     event.json("dyedflames:data_maps/block/fire_types",
         {
-            values: {
-                "supplementaries:lumisene": {
-                    particle: "supplementaries:sparkle",
-                    texture0: "supplementaries:block/lumisene_fire_0",
-                    texture1: "supplementaries:block/lumisene_fire_1"
-                }
-            }
+            values: fireTypesData
         }
-    )
-
-    
+    )    
 })
