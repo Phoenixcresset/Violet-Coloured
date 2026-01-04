ad63d6056276c972b344e25eec307d8409712623
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-04 16:50:28 +0100

    feat: added way to hide items from EMI, and hid Moonlight lib's spawn box

---
+++ kubejs/server_scripts/emi_hide_items.js
@@ -0,0 +1,9 @@
+const itemsToHide = [
+    "moonlight:spawn_box"
+];
+
+RecipeViewerEvents.removeEntries("item", event => {
+    itemsToHide.forEach(item => {
+        event.remove(item);
+    });
+});
