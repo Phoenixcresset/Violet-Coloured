3e09eedf747510d891cb3495eff642fd0eca3b9c
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-17 13:07:34 +0100

    config(horseman): removed Copper Horn

--- kubejs/startup_scripts/obliterate_config.js
+++ kubejs/startup_scripts/obliterate_config.js
@@ -1,5 +1,6 @@
 /** @typedef {Object.<string, Array<string|RegExp>>} ObliteratedPatterns */
 const obliteratedPatterns = {
+  horseman: ["copper_horn"],
   supplementaries: [
     "bamboo_spikes_tipped",
     "cage",
