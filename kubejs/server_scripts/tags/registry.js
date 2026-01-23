1d40edbe998487fab7847f16999459fd79a5798a
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-23 18:49:28 +0100

    refactor(tagModule): moved global module definition to startup

--- kubejs/server_scripts/tags/registry.js
+++ kubejs/server_scripts/tags/registry.js
@@ -1,158 +1,3 @@
-// priority: 10
-/** @typedef {string} ModId */
-/** @typedef {string} ShortTagId */
-/** @typedef {string} ItemId */
-/** @typedef {string} TagId */
-/** @typedef {"item"|"block"|"fluid"|"entity_type"|"worldgen/structure"} TagType */
-/** @typedef {Record<ModId, Array<ShortTagId>>} ModToTagsRecord */
-/** @typedef {Record<TagId, Record<ShortTagId, Array<ItemId>>>} TagToItemsRecord */
-
-global.TagModule = (function () {
-  /** @type {Record<TagType, Array<{tag: TagId, item: ItemId}>>} */
-  const _additionsToItems = {
-    item: [],
-    block: [],
-    fluid: [],
-    entity_type: [],
-    "worldgen/structure": [],
-  };
-  /** @type {Record<TagType, Array<FullTagId>>} */
-  const _removals = {
-    item: [],
-    block: [],
-    fluid: [],
-    entity_type: [],
-    "worldgen/structure": [],
-  };
-  /** @type {Record<TagType, Array<{tag: TagId, item: ItemId}>>} */
-  const _removalsFromItems = {
-    item: [],
-    block: [],
-    fluid: [],
-    entity_type: [],
-    "worldgen/structure": [],
-  };
-
-  /**
-   * @param {ModId} modId
-   * @returns {boolean}
-   */
-  function _isModLoaded(modId) {
-    if (!Platform.isLoaded(modId)) {
-      console.log(`[TagModule] Skipping for ${modId} (mod not loaded)`);
-      return false;
-    }
-    return true;
-  }
-
-  function _formatItemId(modId, itemId) {
-    return itemId.startsWith("#")
-      ? `#${modId}:${itemId.substring(1)}`
-      : `${modId}:${itemId}`;
-  }
-
-//   function _getOrCreateArrayForTagType(record, tagType) {
-//     if (!record[tagType]) {
-//       record[tagType] = [];
-//     }
-//     return record[tagType];
-//   }
-
-  /**
-   * @param {Record<TagType, TagToItemsRecord>} tagsToAddToItems
-   */
-  function registerAddedTagsToItems(tagsToAddToItems) {
-    console.log("[TagModule] Registering added tags to items");
-    for (const [tagType, tagToItemsMap] of Object.entries(tagsToAddToItems)) {
-      for (const [tag, mods] of Object.entries(tagToItemsMap)) {
-        for (const [modId, items] of Object.entries(mods)) {
-          if (!_isModLoaded(modId)) {
-            continue;
-          }
-          for (const item of items) {
-            let formattedItemId = _formatItemId(modId, item);
-            if (_additionsToItems[tagType] === undefined) {
-              _additionsToItems[tagType] = [];
-            }
-            _additionsToItems[tagType].push({
-              tag: tag,
-              item: formattedItemId,
-            });
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * @param {Record<TagType, ModToTagsRecord>} tagsToRemove
-   */
-  function registerRemovedTags(tagsToRemove) {
-    console.log("[TagModule] Registering removed tags");
-    for (const [tagType, modToTagsMap] of Object.entries(tagsToRemove)) {
-      for (const [modId, tags] of Object.entries(modToTagsMap)) {
-        for (const tag of tags) {
-          if (_removals[tagType] === undefined) {
-            _removals[tagType] = [];
-          }
-          _removals[tagType].push(`${modId}:${tag}`);
-        }
-      }
-    }
-  }
-
-  /**
-   * @param {Record<TagType, TagToItemsRecord>} tagsToRemoveFromItems
-   */
-  function registerRemovedTagsFromItems(tagsToRemoveFromItems) {
-    console.log("[TagModule] Registering removed tags from items");
-    for (const [tagType, tagToItemsMap] of Object.entries(
-      tagsToRemoveFromItems,
-    )) {
-      for (const [tag, mods] of Object.entries(tagToItemsMap)) {
-        for (const [modId, items] of Object.entries(mods)) {
-          if (!_isModLoaded(modId)) {
-            continue;
-          }
-          for (const item of items) {
-            let formattedItemId = _formatItemId(modId, item);
-            if (_removalsFromItems[tagType] === undefined) {
-              _removalsFromItems[tagType] = [];
-            }
-            _removalsFromItems[tagType].push({
-              tag: tag,
-              item: formattedItemId,
-            });
-          }
-        }
-      }
-    }
-  }
-
-  function apply(event, tagType) {
-    console.log(`[TagModule] Applying changes to ${tagType} tags`);
-    const additions = _additionsToItems[tagType] || [];
-    const removals = _removals[tagType] || [];
-    const removalsFromItems = _removalsFromItems[tagType] || [];
-    for (const tag of removals) {
-      event.removeAll(tag);
-    }
-    for (const entry of removalsFromItems) {
-      event.remove(entry.tag, entry.item);
-    }
-    for (const entry of additions) {
-      event.add(entry.tag, entry.item);
-    }
-  }
-
-  return {
-    registerAddedTagsToItems: registerAddedTagsToItems,
-    registerRemovedTags: registerRemovedTags,
-    registerRemovedTagsFromItems: registerRemovedTagsFromItems,
-    apply: apply,
-  };
-})();
-
 global.TagModule.registerRemovedTags({
   item: {
     supplementaries: [
@@ -173,6 +18,7 @@ global.TagModule.registerRemovedTags({
     ],
   },
   block: {
+    minecraft: ["enderman_holdable"], // Remove enderman griefing
     supplementaries: [
       "frame_block_blacklist",
       "faucet_connection_blacklist",
@@ -188,6 +34,26 @@ global.TagModule.registerRemovedTags({
       "tickable_when_captured",
     ],
   },
+  "worldgen/structure": {
+    amendments: ["add_potion_cauldron"],
+  },
+});
+
+global.TagModule.registerRemovedTagsFromItems({
+  item: {
+    "curios:belt": {
+      supplementaries: ["#keys", "quiver"],
+    },
+    "trinkets:legs/quiver": {
+      supplementaries: ["quiver"],
+    },
+    "trinkets:legs/key": {
+      supplementaries: ["key"],
+    },
+    "supplementaries:blackboard_black": {
+      minecraft: ["charcoal", "coal"],
+    },
+  },
 });
 
 ServerEvents.tags("item", (event) => {
@@ -200,4 +66,12 @@ ServerEvents.tags("block", (event) => {
 
 ServerEvents.tags("entity_type", (event) => {
   global.TagModule.apply(event, "entity_type");
+});
+
+ServerEvents.tags("fluid", (event) => {
+  global.TagModule.apply(event, "fluid");
+});
+
+ServerEvents.tags("worldgen/structure", (event) => {
+  global.TagModule.apply(event, "worldgen/structure");
 });
---
+++ kubejs/startup_scripts/tags.js
@@ -0,0 +1,157 @@
+/** @typedef {string} ModId */
+/** @typedef {string} ShortTagId */
+/** @typedef {string} ItemId */
+/** @typedef {string} TagId */
+/** @typedef {"item"|"block"|"fluid"|"entity_type"|"worldgen/structure"} TagType */
+/** @typedef {Record<ModId, Array<ShortTagId>>} ModToTagsRecord */
+/** @typedef {Record<TagId, Record<ShortTagId, Array<ItemId>>>} TagToItemsRecord */
+
+global.TagModule = (function () {
+  /** @type {Record<TagType, Array<{tag: TagId, item: ItemId}>>} */
+  const _additionsToItems = {};
+  /** @type {Record<TagType, Array<FullTagId>>} */
+  const _removals = {};
+  /** @type {Record<TagType, Array<{tag: TagId, item: ItemId}>>} */
+  const _removalsFromItems = {};
+
+  /**
+   * @param {ModId} modId
+   * @returns {boolean}
+   */
+  function _isModLoaded(modId) {
+    if (!Platform.isLoaded(modId)) {
+      console.log(`[TagModule] Skipping for ${modId} (mod not loaded)`);
+      return false;
+    }
+    return true;
+  }
+
+  function _formatItemId(modId, itemId) {
+    return itemId.startsWith("#")
+      ? `#${modId}:${itemId.substring(1)}`
+      : `${modId}:${itemId}`;
+  }
+
+  function _getOrCreateArrayForTagType(record, tagType) {
+    if (!record[tagType]) {
+      record[tagType] = [];
+    }
+    return record[tagType];
+  }
+
+  /**
+   * @param {Record<TagType, TagToItemsRecord>} tagsToAddToItems
+   */
+  function registerAddedTagsToItems(tagsToAddToItems) {
+    for (const [tagType, tagToItemsMap] of Object.entries(tagsToAddToItems)) {
+      let additionsSet = new Set(
+        (_additionsToItems[tagType] || []).map(
+          (entry) => `${entry.tag}|${entry.item}`,
+        ),
+      );
+
+      for (const [tag, mods] of Object.entries(tagToItemsMap)) {
+        for (const [modId, items] of Object.entries(mods)) {
+          if (!_isModLoaded(modId)) {
+            continue;
+          }
+          for (const item of items) {
+            let formattedItemId = _formatItemId(modId, item);
+            if (additionsSet.has(`${tag}|${formattedItemId}`)) {
+              continue;
+            }
+            _getOrCreateArrayForTagType(_additionsToItems, tagType).push({
+              tag: tag,
+              item: formattedItemId,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Record<TagType, ModToTagsRecord>} tagsToRemove
+   */
+  function registerRemovedTags(tagsToRemove) {
+    for (const [tagType, modToTagsMap] of Object.entries(tagsToRemove)) {
+      let removalsSet = new Set(_removals[tagType] || []);
+
+      for (const [modId, tags] of Object.entries(modToTagsMap)) {
+        for (const tag of tags) {
+          let fullTagId = `${modId}:${tag}`;
+          if (removalsSet.has(fullTagId)) {
+            continue;
+          }
+          _getOrCreateArrayForTagType(_removals, tagType).push(fullTagId);
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Record<TagType, TagToItemsRecord>} tagsToRemoveFromItems
+   */
+  function registerRemovedTagsFromItems(tagsToRemoveFromItems) {
+    for (const [tagType, tagToItemsMap] of Object.entries(
+      tagsToRemoveFromItems,
+    )) {
+      // Using a keyed set to avoid duplicates, since removalsFromItems contains objects
+      let removalsFromItemsSet = new Set(
+        (_removalsFromItems[tagType] || []).map(
+          (entry) => `${entry.tag}|${entry.item}`,
+        ),
+      );
+
+      for (const [tag, mods] of Object.entries(tagToItemsMap)) {
+        for (const [modId, items] of Object.entries(mods)) {
+          if (!_isModLoaded(modId)) {
+            continue;
+          }
+          for (const item of items) {
+            let formattedItemId = _formatItemId(modId, item);
+            if (removalsFromItemsSet.has(`${tag}|${formattedItemId}`)) {
+              continue;
+            }
+            _getOrCreateArrayForTagType(_removalsFromItems, tagType).push({
+              tag: tag,
+              item: formattedItemId,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  function apply(event, tagType) {
+    const additions = _additionsToItems[tagType] || [];
+    const removals = _removals[tagType] || [];
+    const removalsFromItems = _removalsFromItems[tagType] || [];
+    for (const tag of removals) {
+      event.removeAll(tag);
+    }
+    for (const entry of removalsFromItems) {
+      event.remove(entry.tag, entry.item);
+    }
+    for (const entry of additions) {
+      event.add(entry.tag, entry.item);
+    }
+  }
+
+  function getRemovedTags() {
+    return _removals;
+  }
+
+  function getRemovedTagsFromItems() {
+    return _removalsFromItems;
+  }
+
+  return {
+    registerAddedTagsToItems: registerAddedTagsToItems,
+    registerRemovedTags: registerRemovedTags,
+    registerRemovedTagsFromItems: registerRemovedTagsFromItems,
+    apply: apply,
+    getRemovedTags: getRemovedTags,
+    getRemovedTagsFromItems: getRemovedTagsFromItems,
+  };
+})();
