918c1df0cf7800fad059d534f8b5c66d904c5129
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-25 19:17:42 +0100

    refactor(Farmer's Delight): Changed Mushroom Colony Info adding

--- kubejs/server_scripts/emi_info.js
+++ kubejs/server_scripts/emi_info.js
@@ -1,9 +1,10 @@
 RecipeViewerEvents.addInformation("item", (event) => {
-  event.add("farmersdelight:red_mushroom_colony", [
-    Text.translatable("emi.info.mushroom_colony"),
-  ]);
-  event.add("farmersdelight:brown_mushroom_colony", [
-    Text.translatable("emi.info.mushroom_colony"),
-  ]);
+  event.add(
+    [
+      "farmersdelight:red_mushroom_colony",
+      "farmersdelight:brown_mushroom_colony",
+    ],
+    [Text.translatable("emi.info.mushroom_colony")],
+  );
   event.add("farmersdelight:tomato", [Text.translatable("emi.info.tomatoes")]);
 });
