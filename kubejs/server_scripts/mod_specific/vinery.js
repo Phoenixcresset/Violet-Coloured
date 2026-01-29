7e44c4b0285c6977a54da3686ac167fa2a5f9b34
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-29 13:33:43 +0100

    feat(Vinery): removed apple drops from oak and dark oak leaves

--- kubejs/server_scripts/mod_specific/vinery.js
+++ kubejs/server_scripts/mod_specific/vinery.js
@@ -468,4 +468,11 @@
         .id(`vinery:apple_mashing/${recipe.outputId.split(":")[1]}`);
     }
   });
+
+  // Removes apple from oak leaves drop table, since Vinery adds apple trees
+  LootJS.lootTables((event) => {
+    event
+      .modifyLootTables(/minecraft:blocks\/.*oak_leaves/)
+      .removeItem("minecraft:apple");
+  });
 })();
