406d6321712f1e602cda837b3c8bf1bd7e08ea3c
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-30 13:01:06 +0100

    feat(Hearth and Harvest): added consistency with BnC's cheeses

--- kubejs/server_scripts/tags/tags.js
+++ kubejs/server_scripts/tags/tags.js
@@ -4,6 +4,12 @@
       "dummmmmmy:arthropod_heads": {
         supplementaries: ["spider_head"],
       },
+      "hearthandharvest:cheese_slices": {
+        brewinandchewin: ["flaxen_cheese_wedge", "scarlet_cheese_wedge"],
+      },
+      "brewinandchewin:foods/cheese_wedge": {
+        hearthandharvest: ["cheddar_cheese_slice", "goat_cheese_slice"],
+      },
     },
   };
 
