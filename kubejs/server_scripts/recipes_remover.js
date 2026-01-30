7fc2812e0f1e0246f7f7cee56f12453f111505b2
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-30 13:01:47 +0100

    chore(HnH): deleted redundant recipes

--- kubejs/server_scripts/recipes_remover.js
+++ kubejs/server_scripts/recipes_remover.js
@@ -1,6 +1,10 @@
 const removedRecipesIds = [
   "supplementaries:cannon_boat_bamboo",
   "minecraft:cake",
+  "hearthandharvest:cake_from_batter",
+  "hearthandharvest:carrot_cake_from_batter",
+  "farmersdelight:cooking/unripe_cheddar_cheese_wheel",
+  "farmersdelight:cooking/unripe_goat_cheese_wheel",
 ];
 
 const removedRecipesTypes = ["vinery:wine_fermentation"];
