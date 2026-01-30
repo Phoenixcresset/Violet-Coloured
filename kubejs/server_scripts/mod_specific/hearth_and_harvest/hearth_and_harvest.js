d208914cd1cefd59e969dafbb3af7e3ed244994d
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-30 13:32:30 +0100

    feat(Hearth and Harvest): removed unused features

---
+++ kubejs/server_scripts/mod_specific/hearth_and_harvest/hearth_and_harvest.js
@@ -0,0 +1,19 @@
+(() => {
+  ServerEvents.generateData("after_mods", (event) => {
+    const biomeModifiersToRemove = [
+      "add_mums",
+      "blueberry_bush",
+      "raspberry_bush",
+      "wild_cotton",
+      "wild_green_grapes",
+      "wild_peanuts",
+      "wild_red_grapes",
+    ];
+
+    for (const path of biomeModifiersToRemove) {
+      global
+        .DataGenModule()
+        .removeData(event, "biome_modifier", "hearthandharvest", path);
+    }
+  });
+})();
