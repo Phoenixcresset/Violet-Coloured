f1705594c02e5eec0cf7fdfddf8b7af6ca68f152
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-01-30 13:58:47 +0100

    feat(Hearth and Harvest): removed corn maze structure

--- kubejs/server_scripts/mod_specific/hearth_and_harvest/hearth_and_harvest.js
+++ kubejs/server_scripts/mod_specific/hearth_and_harvest/hearth_and_harvest.js
@@ -10,13 +10,18 @@
       "wild_red_grapes",
     ];
 
+    const structureToRemove = "corn_maze";
     for (const path of biomeModifiersToRemove) {
       global
         .DataGenModule()
         .removeData(event, "biome_modifier", "hearthandharvest", path);
     }
-    LootJS.lootTables((event) => {
-      event.getLootTable("hearthandharvest:chests/corn_maze").clear();
-    });
+    global
+      .DataGenModule()
+      .removeData(event, "structure_set", "hearthandharvest", structureToRemove);
+  });
+
+  LootJS.lootTables((event) => {
+    event.getLootTable("hearthandharvest:chests/corn_maze").clear();
   });
 })();
