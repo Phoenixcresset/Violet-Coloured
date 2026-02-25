4c196c232babfc2a78521003bb7bb4a27a4b3ab9
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-02-25 17:11:45 +0100

    fix(Quark): prevent using an oretoise spawn egg on an oretoise
    It used to spawn a dead oretoise

---
+++ kubejs/client_scripts/event_cancel/oretoise.js
@@ -0,0 +1,4 @@
+ItemEvents.entityInteracted("quark:toretoise_spawn_egg", (event) => {
+  if (!(event.getTarget().getType() === "quark:toretoise")) return;
+  event.cancel();
+});
---
+++ kubejs/server_scripts/mod_specific/quark/oretoise.js
@@ -0,0 +1,3 @@
+ItemEvents.entityInteracted("quark:toretoise_spawn_egg", (event) => {
+  if (event.getTarget().getType() === "quark:toretoise") event.cancel();
+});
