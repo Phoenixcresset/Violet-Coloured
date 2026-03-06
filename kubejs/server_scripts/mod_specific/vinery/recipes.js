dd30e88667c2239a4ebc839f6e5c39b114e710c9
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-06 10:59:50 +0100

    fix(Vinery): change slab recipes from shapeless to shaped

--- kubejs/server_scripts/mod_specific/vinery/recipes.js
+++ kubejs/server_scripts/mod_specific/vinery/recipes.js
@@ -1,5 +1,31 @@
 (() => {
+  const slabRecipes = [
+    {
+      ingredient: "minecraft:dirt",
+      output: "vinery:dirt_slab",
+    },
+    {
+      ingredient: "minecraft:grass_block",
+      output: "vinery:grass_slab",
+    },
+    {
+      ingredient: "minecraft:coarse_dirt",
+      output: "vinery:coarse_dirt_slab",
+    },
+  ];
+  function slabRecipe(event, ingredient, output) {
+    event
+      .shaped(Item.of(output, 6), ["BBB"], {
+        B: ingredient,
+      })
+      .id(output);
+  }
   ServerEvents.recipes((event) => {
+    for (const recipe of slabRecipes) {
+      slabRecipe(event, recipe.ingredient, recipe.output);
+    }
+
+    // TODO Extract shaped recipe
     // Replaces the default recipe to avoid overlap with Quark posts
     event
       .shaped("vinery:grapevine_stem", ["S", "S", "S"], {
