f2ca6817010c82f71ac074d6a8a5c4bb732ec340
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-06 11:41:27 +0100

    refactor(Quark): extract recipe modifications in objects and functions

--- kubejs/server_scripts/mod_specific/quark/recipes.js
+++ kubejs/server_scripts/mod_specific/quark/recipes.js
@@ -1,48 +1,81 @@
 (() => {
-  ServerEvents.recipes((event) => {
-    for (const color of Color.DYE.values()) {
-      event
-        .shaped(
-          Item.of(`quark:${color}_framed_glass`, 8),
-          ["GGG", "GDG", "GGG"],
-          {
-            G: "quark:framed_glass",
+  const surroundedRecipes = [
+    {
+      ingredient: "minecraft:glass",
+      surroundedItem: "supplementaries:ash",
+      output: "quark:dirty_glass",
+      recipeId: "quark:tweaks/crafting/glass/mixed_dirty_glass",
+    },
+    {
+      ingredient: "minecraft:glass_pane",
+      surroundedItem: "supplementaries:ash",
+      output: "quark:dirty_glass_pane",
+      recipeId: "quark:tweaks/crafting/glass/dirty_glass_pane_from_glass_pane",
+    },
+  ];
 
-            D: `minecraft:${color}_dye`,
-          }
-        )
-        .id(`quark:building/crafting/glass/${color}_framed_glass`);
-      event
-        .shaped(
-          Item.of(`quark:${color}_framed_glass_pane`, 8),
-          ["PPP", "PDP", "PPP"],
-          {
-            P: "quark:framed_glass_pane",
-            D: `minecraft:${color}_dye`,
-          }
-        )
-        .id(
-          `quark:building/crafting/panes/${color}_framed_glass_pane_from_framed_glass_pane`
-        );
+  const dyedRecipes = [
+    {
+      ingredient: "quark:framed_glass",
+      output: "quark:{color}_framed_glass",
+      recipeId: "quark:building/crafting/glass/{color}_framed_glass",
+    },
+    {
+      ingredient: "quark:framed_glass_pane",
+      output: "quark:{color}_framed_glass_pane",
+      recipeId:
+        "quark:building/crafting/panes/{color}_framed_glass_pane_from_framed_glass_pane",
+    },
+  ];
+
+  const removedRecipesIds = [
+    // Removed to avoid recipe conflicts
+    "quark:building/crafting/chests/mixed_chest",
+    "quark:building/crafting/chests/mixed_chest_wood",
+    "minecraft:furnace",
+  ];
+
+  function surroundWithItemRecipe(
+    event,
+    ingredient,
+    surroundedItem,
+    output,
+    recipeId
+  ) {
+    event
+      .shaped(Item.of(output, 8), ["III", "ISI", "III"], {
+        I: ingredient,
+        S: surroundedItem,
+      })
+      .id(recipeId);
+  }
+
+  function dyedRecipe(event, ingredient, output, recipeId) {
+    for (const color of Color.DYE.values()) {
+      let dyedOutput = output.replace("{color}", color);
+      let dyedRecipeId = recipeId.replace("{color}", color);
+      let dye = `minecraft:${color}_dye`;
+      surroundWithItemRecipe(event, ingredient, dye, dyedOutput, dyedRecipeId);
+    }
+  }
+
+  ServerEvents.recipes((event) => {
+    for (const recipe of surroundedRecipes) {
+      surroundWithItemRecipe(
+        event,
+        recipe.ingredient,
+        recipe.surroundedItem,
+        recipe.output,
+        recipe.recipeId
+      );
     }
 
-    // Removed to avoid recipe conflicts
-    event.remove({ id: "quark:building/crafting/chests/mixed_chest" });
-    event.remove({ id: "quark:building/crafting/chests/mixed_chest_wood" });
-    event.remove({ id: "minecraft:furnace" });
+    for (const recipe of dyedRecipes) {
+      dyedRecipe(event, recipe.ingredient, recipe.output, recipe.recipeId);
+    }
 
-    event
-      .shaped("quark:dirty_glass", ["GGG", "GAG", "GGG"], {
-        G: "minecraft:glass",
-        A: "supplementaries:ash",
-      })
-      .id("quark:tweaks/crafting/glass/mixed_dirty_glass");
-
-    event
-      .shaped("quark:dirty_glass_pane", ["PPP", "PAP", "PPP"], {
-        P: "minecraft:glass_pane",
-        A: "supplementaries:ash",
-      })
-      .id("quark:tweaks/crafting/glass/dirty_glass_pane_from_glass_pane");
+    for (const recipeId of removedRecipesIds) {
+      event.remove({ id: recipeId });
+    }
   });
 })();
