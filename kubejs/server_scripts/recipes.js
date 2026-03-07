caab886cd73aa1c918655a6f86d4b0244b1d68b6
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-07 10:35:03 +0100

    tweak(Vanilla): change paper, coookie and bread recipes to be shapeless

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -7,9 +7,46 @@
     },
   ];
 
+  const shapelessRecipes = [
+    {
+      ingredients: [
+        "minecraft:cocoa_beans",
+        "minecraft:wheat",
+        "minecraft:wheat",
+      ],
+      output: "minecraft:cookie",
+      outputCount: 8,
+      recipeId: "minecraft:cookie",
+    },
+    {
+      ingredients: ["minecraft:wheat", "minecraft:wheat", "minecraft:wheat"],
+      output: "minecraft:bread",
+      outputCount: 1,
+      recipeId: "minecraft:bread",
+    },
+    {
+      ingredients: [
+        "minecraft:sugar_cane",
+        "minecraft:sugar_cane",
+        "minecraft:sugar_cane",
+      ],
+      output: "minecraft:paper",
+      outputCount: 3,
+      recipeId: "minecraft:paper",
+    },
+  ];
+
   ServerEvents.recipes((event) => {
     for (const recipe of replacedInputRecipes) {
       event.replaceInput(recipe.filter, recipe.old_input, recipe.new_input);
+    }
+    for (const recipe of shapelessRecipes) {
+      event
+        .shapeless(
+          Item.of(recipe.output, recipe.outputCount),
+          recipe.ingredients
+        )
+        .id(recipe.recipeId);
     }
   });
 })();
