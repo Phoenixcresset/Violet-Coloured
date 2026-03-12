ca7a8f79944a24ab9ef7e5c2ede70621c177b350
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-12 18:24:57 +0100

    tweak(HnH): remove tag deletion as tappable tag was fixed in HnH 1.1.3

--- kubejs/server_scripts/mod_specific/hearth_and_harvest/tags.js
+++ kubejs/server_scripts/mod_specific/hearth_and_harvest/tags.js
@@ -27,15 +27,5 @@
     },
   };
 
-  const removedTagsFromEntries = {
-    block: {
-      "hearthandharvest:tappable": {
-        biomesoplenty: ["fir_log", "maple_log", "pine_log"],
-        autumnity: ["maple_log", "sappy_maple_log"], // TODO: remove if/when Autumnity is added to the modpack, but for now this is needed to prevent errors from the missing tag
-      },
-    },
-  };
-
   global.TagModule.registerAddedTagsToEntries(addedTagsToEntries);
-  global.TagModule.registerRemovedTagsFromEntries(removedTagsFromEntries);
 })();
