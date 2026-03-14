8a4fb4bfebeea76d0d6ea5fdd62f79c84e30700f
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-14 08:42:54 +0100

    tweak(Vinery): remove Winemaker and Wandering Winemaker

--- kubejs/startup_scripts/obliterate_config.js
+++ kubejs/startup_scripts/obliterate_config.js
@@ -138,6 +138,8 @@
       /winemaker.*/,
       /.*_grapejuice/,
       "apple_juice",
+      "wandering_winemaker_spawn_egg",
+      "mule_spawn_egg",
       /.*_wine_rack_mid/, // Not intuitive to use, can be replaced by just placing the wine down (works with aging)
       // Storage blocks replaced by Crate Delight or Hearth and Harvest
       "apple_bag",
