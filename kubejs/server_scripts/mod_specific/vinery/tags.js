c44964b2cd313cb09ff826b9d478f3ac17a5c146
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-16 21:34:23 +0100

    tweak(Vinery): remove tags and add relevant tags to items

--- kubejs/server_scripts/mod_specific/vinery/tags.js
+++ kubejs/server_scripts/mod_specific/vinery/tags.js
@@ -17,6 +17,75 @@
       "c:foods/food_poisoning": {
         vinery: ["rotten_cherry"],
       },
+      "minecraft:hanging_signs": {
+        vinery: ["dark_cherry_hanging_sign"],
+      },
+      "minecraft:slabs": {
+        vinery: [
+          "dirt_slab",
+          "grass_slab",
+          "dirt_path_slab",
+          "coarse_dirt_slab",
+        ],
+      },
+      "c:stripped_logs": {
+        vinery: ["stripped_dark_cherry_log"],
+      },
+      "c:stripped_woods": {
+        vinery: ["stripped_dark_cherry_wood"],
+      },
+      "c:fence_gates/wooden": {
+        vinery: ["dark_cherry_fence_gate"],
+      },
+      "minecraft:fence_gates": {
+        vinery: ["dark_cherry_fence_gate"],
+      },
+      "farmersdelight:cabinets/wooden": {
+        vinery: ["dark_cherry_cabinet"],
+      },
+      "c:seeds": {
+        vinery: ["red_grape_seeds", "white_grape_seeds"],
+      },
+    },
+    block: {
+      "minecraft:ceiling_hanging_signs": {
+        vinery: ["dark_cherry_hanging_sign"],
+      },
+      "minecraft:wall_hanging_signs": {
+        vinery: ["dark_cherry_wall_hanging_sign"],
+      },
+      "minecraft:standing_signs": {
+        vinery: ["dark_cherry_sign"],
+      },
+      "minecraft:wall_signs": {
+        vinery: ["dark_cherry_wall_sign"],
+      },
+      "minecraft:slabs": {
+        vinery: [
+          "dirt_slab",
+          "grass_slab",
+          "dirt_path_slab",
+          "coarse_dirt_slab",
+        ],
+      },
+      "c:stripped_logs": {
+        vinery: ["stripped_dark_cherry_log"],
+      },
+      "c:stripped_woods": {
+        vinery: ["stripped_dark_cherry_wood"],
+      },
+      "minecraft:overworld_natural_logs": {
+        vinery: ["dark_cherry_log"],
+      },
+      "minecraft:snaps_goat_horn": {
+        vinery: ["dark_cherry_log"],
+      },
+      "c:fence_gates/wooden": {
+        vinery: ["dark_cherry_fence_gate"],
+      },
+      "minecraft:fence_gates": {
+        vinery: ["dark_cherry_fence_gate"],
+      },
     },
   };
   const removedTagsFromEntries = {
@@ -26,8 +95,36 @@
         vinery: ["white_grape", "red_grape"],
       },
     },
+    block: {
+      "minecraft:wall_signs": {
+        vinery: ["dark_cherry_sign"],
+      },
+    },
+  };
+
+  const removedTags = {
+    item: {
+      vinery: [
+        "ignore_block_item",
+        "white_wine",
+        "red_wine",
+        "hanging_signs",
+        "grape_seeds",
+        "red_grape",
+        "white_grape",
+      ],
+    },
+    block: {
+      vinery: [
+        "ceiling_hanging_signs",
+        "wall_hanging_signs",
+        "all_hanging_signs",
+        "can_not_connect",
+      ],
+    },
   };
 
   global.TagModule.registerAddedTagsToEntries(addedTagsToEntries);
   global.TagModule.registerRemovedTagsFromEntries(removedTagsFromEntries);
+  global.TagModule.registerRemovedTags(removedTags);
 })();
