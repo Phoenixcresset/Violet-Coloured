4f001c638822c1166640e293da732100ca745974
Phoenixcresset <86055389+Phoenixcresset@users.noreply.github.com>
2026-03-17 15:09:23 +0100

    refactor: replace EMI info from KJS function to assets generation

---
+++ kubejs/client_scripts/emi_info.js
@@ -0,0 +1,32 @@
+(() => {
+  const informations = [
+    {
+      stacks: [
+        "item:farmersdelight:red_mushroom_colony",
+        "item:farmersdelight:brown_mushroom_colony",
+      ],
+      translationKey: "emi.info.mushroom_colony",
+    },
+    {
+      stacks: ["item:farmersdelight:tomato"],
+      translationKey: "emi.info.tomatoes",
+    },
+    {
+      stacks: ["item:vinery:red_grape", "item:vinery:white_grape"],
+      translationKey: "hearthandharvest.jei.info.wild_grapes",
+    },
+  ];
+
+  ClientEvents.generateAssets("after_mods", (event) => {
+    for (const information of informations) {
+      let informationRecipe = {
+        type: "emi:info",
+        stacks: information.stacks,
+        text: information.translationKey,
+      };
+      let recipeId = information.translationKey.replace(/\./g, "_");
+      console.log(recipeId);
+      event.json(`emi:recipe/additions/${recipeId}`, informationRecipe);
+    }
+  });
+})();
